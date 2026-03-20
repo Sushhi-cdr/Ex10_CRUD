24b8a702541607fb3692bdcbd1839e07c8f477a6
Sushhi-cdr <enzoseiti@gmail.com>
2026-03-20 19:45:12 -0300

    Criação do services

---
+++ src/main/java/com/example/Ex10/Services/ServicoTarefa.java
@@ -0,0 +1,31 @@
+package com.example.Ex10.Services;
+
+import com.example.Ex10.Models.Tarefa;
+import com.example.Ex10.Repositories.RepositorioTarefa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServicoTarefa {
+
+    @Autowired
+    private RepositorioTarefa repositorio;
+
+    public Tarefa criar(Tarefa tarefa){
+        return repositorio.save(tarefa);
+    }
+
+    public List<Tarefa> findAll(){
+        return repositorio.findAll();
+    }
+
+    public Tarefa buscarPorId(Long id){
+        return repositorio.findById(id).orElse(null);
+    }
+
+    public void deletar(Long id){
+        repositorio.deleteById(id);
+    }
+}
